c0b2c77150f923bfc8862937b104eccf0fcfb95b
Pinny Geliebter <pgibralter@gmail.com>
2022-01-10 23:01:31 -0500

    removes variable setting

--- app/models/github_profile_scraper.rb
+++ app/models/github_profile_scraper.rb
@@ -9,22 +9,22 @@ class GithubProfileScraper
   end
 
   def this_year_commits
-    year_commits = @doc.css("h2.f4.text-normal.mb-2").text.match(/\b\d+\b/)[0].to_i
+    @doc.css("h2.f4.text-normal.mb-2").text.match(/\b\d+\b/)[0].to_i
   end
 
   def this_week_commits
-    this_week_commits = @doc.css(".js-calendar-graph-svg").css("g").css("g")[-1].css("rect").reduce(0) { |sum, day|
+    @doc.css(".js-calendar-graph-svg").css("g").css("g")[-1].css("rect").reduce(0) { |sum, day|
       sum + day.attributes["data-count"].value.to_i
     }
   end
 
   def last_week_commits
-    last_week_commits = @doc.css(".js-calendar-graph-svg").css("g").css("g")[-2].css("rect").reduce(0) { |sum, day|
+    @doc.css(".js-calendar-graph-svg").css("g").css("g")[-2].css("rect").reduce(0) { |sum, day|
       sum + day.attributes["data-count"].value.to_i
     }
   end
 
   def todays_commits
-    todays_commits = @doc.css(".js-calendar-graph-svg").css("g").css("g")[-1].css("rect").find { |day| day.attributes["data-date"].value == Date.today.strftime("%Y-%m-%d") }.attributes["data-count"].value.to_i
+    @doc.css(".js-calendar-graph-svg").css("g").css("g")[-1].css("rect").find { |day| day.attributes["data-date"].value == Date.today.strftime("%Y-%m-%d") }.attributes["data-count"].value.to_i
   end
 end
